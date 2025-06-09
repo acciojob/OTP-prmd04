f911a21e2d52dd71cee8f60ce1dc63f10f211c81
Pramod Ade <109603626+prmd04@users.noreply.github.com>
2025-06-09 20:21:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-//your JS code here. If required.
 const inputs = document.querySelectorAll('.code');
 
-inputs.forEach((input, index) => {
+inputs.forEach((input, idx) => {
   input.addEventListener('input', (e) => {
     const value = e.target.value;
-    if (value.length ===1 && index < inputs.length - 1) {
-      inputs[index + 1].focus();
+    if (value && idx < inputs.length - 1) {
+      inputs[idx + 1].focus();
     }
   });
 
   input.addEventListener('keydown', (e) => {
     if (e.key === 'Backspace') {
-      if (input.value === '' && index > 0) {
-        inputs[index - 1].focus();
-        inputs[index - 1].value = ''; // optional: clear previous input
+      if (input.value) {
+        input.value = '';
+      } else if (idx > 0) {
+        inputs[idx - 1].focus();
+        inputs[idx - 1].value = '';
       }
     }
   });
