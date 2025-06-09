de37b2d05bc057264c312d5c2a49e6a802b6f925
Pramod Ade <109603626+prmd04@users.noreply.github.com>
2025-06-09 20:28:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
-const inputs = document.querySelectorAll('.code');
+const inputs = document.querySelectorAll(".code");
+
+inputs[0].focus(); // focus first input on page load
 
 inputs.forEach((input, idx) => {
-  input.addEventListener('input', (e) => {
+  input.addEventListener("input", (e) => {
     const value = e.target.value;
-    if (value && idx < inputs.length - 1) {
-      inputs[idx + 1].focus();
+
+    if (value.match(/^[0-9]$/)) {
+      if (idx < inputs.length - 1) {
+        inputs[idx + 1].focus();
+      }
+    } else {
+      e.target.value = ""; // allow only digits
     }
   });
 
-  input.addEventListener('keydown', (e) => {
-    if (e.key === 'Backspace') {
-      if (input.value) {
-        input.value = '';
-      } else if (idx > 0) {
-        inputs[idx - 1].focus();
-        inputs[idx - 1].value = '';
+  input.addEventListener("keydown", (e) => {
+    if (e.key === "Backspace") {
+      if (input.value === "") {
+        if (idx > 0) {
+          inputs[idx - 1].focus();
+          inputs[idx - 1].value = "";
+        }
       }
     }
   });
